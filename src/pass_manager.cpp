960b54c1b105d58f6ae72ccd5ecbdc8170a6f185
Shucai Xiao <Shucai.Xiao@amd.com>
2019-04-02 16:05:07 -0500

    clang format

--- src/pass_manager.cpp
+++ src/pass_manager.cpp
@@ -16,13 +16,14 @@
 namespace migraphx {
 inline namespace MIGRAPHX_INLINE_NS {
 
-void run_passes(program& prog, std::vector<pass> passes, tracer trace) {
-    if (enabled(MIGRAPHX_TRACE_COMPILE{}))
+void run_passes(program& prog, std::vector<pass> passes, tracer trace)
+{
+    if(enabled(MIGRAPHX_TRACE_COMPILE{}))
         trace = tracer{std::cout};
 
     trace(prog);
     trace();
-    for (auto p : passes)
+    for(auto p : passes)
     {
         trace("Pass: ", p.name());
         p.apply(prog);
